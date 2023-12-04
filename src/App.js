b2ba4d1d4b9fe5d0d320813b280dabde8864b38e
dCOMMICS <sanwurld@gmail.com>
2023-12-04 16:47:20 +0300

    dynamic links added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "bootstrap/dist/js/bootstrap";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center"> Ricky & Morty <span className="text-primary">Wiki</span></h1>
+      <h1 className="text-center"> 
+      Rick & Morty <span className="text-primary">Wiki</span>
+      </h1>
     </div>
   );
 }

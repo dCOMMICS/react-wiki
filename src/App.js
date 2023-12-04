36cf62e73d85cc9ea6674409cfd34481834fa3e5
dCOMMICS <sanwurld@gmail.com>
2023-12-04 16:15:24 +0300

    💪🏿

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/js/bootstrap";
 
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center"> Ricky & Morty</h1>
+      <h1 className="text-center"> Ricky & Morty <span className="text-primary">Wiki</span></h1>
     </div>
   );
 }
